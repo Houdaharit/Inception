3836dd387215b4956e40fa1a056eca6fb881bdab
houdaharit <houdaharit20@gmail.com>
2023-05-24 17:28:08 +0100

    I need a here

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,3 +1,5 @@
-sed -i 's/^listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
-sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php/7.3/fpm/php.ini
-
+mysql
+CREATE DATABASE wordpressdb;
+CREATE USER IF NOT EXISTS 'hharit'@'hharit.42.fr' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON testdb.* TO 'hharit'@'hharit.42.fr';
+FLUSH PRIVILEGES;
