fa5bca572f8639dc9c195fa943fa3374f4f0a328
houdaharit <houdaharit20@gmail.com>
2023-05-26 17:10:58 +0100

    root inaccessible without password

--- srcs/requirements/mariadb/tools/script.sql
+++ srcs/requirements/mariadb/tools/script.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE wordpressdb default character set utf8 collate utf8_unicode_ci;
 CREATE USER IF NOT EXISTS 'hharit'@'%' IDENTIFIED BY 'qwerty1234';
 GRANT ALL PRIVILEGES ON wordpressdb.* TO 'hharit'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;
