b5484c9e427ffd5dac121ff418a81144e8180b59
Houda Harit <houdaharit20@gmail.com>
2023-06-04 01:17:52 +0100

    changed env

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-all: build
+all: build up
 
 build:
-	echo "127.0.0.1 hharit.42.fr" | sudo tee -a /etc/hosts
-	mkdir -p /home/hharit/data/wordpress
-	mkdir -p /home/hharit/data/mariadb
-	docker-compose -f ./srcs/docker-compose.yaml up --build
+	#echo "127.0.0.1 hharit.42.fr" | sudo tee -a /etc/hosts
+	mkdir -p /Users/hharit/data/wordpress
+	mkdir -p /Users/hharit/data/mariadb
+	docker-compose -f ./srcs/docker-compose.yaml build
 
 up:
 	docker-compose -f ./srcs/docker-compose.yaml up
@@ -12,10 +12,13 @@ up:
 down:
 	docker-compose -f ./srcs/docker-compose.yaml down
 
+stop:
+	docker-compose -f ./srcs/docker-compose.yaml stop
+
 clean:
 	docker-compose -f ./srcs/docker-compose.yaml down --rmi all
 	docker volume rm $$(docker volume ls -q);\
-	sudo rm -rf /home/hharit/data
+	rm -rf /Users/hharit/data
 
 fclean:
 	docker system prune --force --all
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,10 +1,10 @@
-sed -i "s/WP_DB_PASSWORD/"$WP_DB_PASSWORD"/" wp-config.php
-sed -i "s/WP_DB_USERNAME/"$WP_DB_USERNAME"/" wp-config.php
+sed -i "s/WP_DB_PASSWORD/"$MYSQL_PASSWORD"/" wp-config.php
+sed -i "s/WP_DB_USERNAME/"$MYSQL_USER"/" wp-config.php
 sed -i "s/WP_DB_HOST/"$WP_DB_HOST"/" wp-config.php
 sed -i "s/WP_DB/"$WP_DB"/" wp-config.php
 
 cd /var/www/html/wordpress
-wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
-wp user create $USER $USER_EMAIL --role=author --user_pass=$USER_PASSWORD --allow-root
+wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+wp user create $SECOND_USER $SECOND_USER_EMAIL --role=author --user_pass=$SECOND_USER_PASSWORD --allow-root
 
 php-fpm7.3 -F -R
